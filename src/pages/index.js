bff71c2d055caf6c044ddd90e46496c35b97cc9d
Ga Jun <gajunyoung1997@gmail.com>
2021-10-09 20:25:44 +0100

    Testing deployment using github actions

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>Testing deployment works with workflow</p>
     {/* <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
