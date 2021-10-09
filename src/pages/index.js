3cafa3ce1f3ba3f18d9e651adc59703f88eb4980
Ga Jun <gajunyoung1997@gmail.com>
2021-10-09 21:18:31 +0100

    testing small changes to webpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <p>Testing deployment works with workflow</p>
+    <p>updating more content for testing after workflow success</p>
     {/* <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
